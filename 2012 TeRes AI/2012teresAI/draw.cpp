dbe1b65f7cd6daba02cd00438121b4d3c26a0d32
matsushin1222251 <matsushin122251@tbz.t-com.co.jp>
2012-10-02 12:52:18 +0900

    Pキーでのマップ表示改善

--- 2012 TeRes AI/2012teresAI/draw.cpp
+++ 2012 TeRes AI/2012teresAI/draw.cpp
@@ -29,7 +29,7 @@ void draw(int stage[WIDTH][HEIGHT],AI_T ai[AI_NUM],Tagger tagger){
 	//AIの描画
 	for(int i=0;i<AI_NUM;i++){
 		SetDrawBright(255,255,255);
-		DrawRotaGraph(ai[i].s_x,ai[i].s_y,1,0,ai[i].Graph,TRUE,FALSE);//読み込んだ画像表示
+		DrawRotaGraph(ai[i].s_x,ai[i].s_y,0.5,0,ai[i].Graph,TRUE,FALSE);//読み込んだ画像表示
 		//DrawCircle(ai[i].s_x,ai[i].s_y,5,GetColor(255,0,0),1);//点表示
 		if(view==1){
 			DrawBox(BOX*ai[i].x,BOX*ai[i].y,BOX*(ai[i].x+1),BOX*(ai[i].y+1),GetColor(255,0,0),0);
@@ -54,12 +54,37 @@ void draw(int stage[WIDTH][HEIGHT],AI_T ai[AI_NUM],Tagger tagger){
 	
 	//マップデータ表示
 	while(CheckHitKey(KEY_INPUT_P)){
+		int ai_x[AI_NUM],ai_y[AI_NUM],ai_num=0;
+		DrawBox(0,0,640,480,GetColor(0,0,0),1);
 		for(int i=0;i<WIDTH;i++){
 			for(int j=0;j<HEIGHT;j++){
-				int cr=GetColor(100,255,100);
+				int cr;
+				switch(stage[i][j]){
+				case 1:
+					cr=GetColor(100,50,0);
+					break;
+				case 2:
+					cr=GetColor(255,0,0);
+					ai_x[ai_num]=i;
+					ai_y[ai_num]=j;
+					ai_num++;
+					break;
+				case 3:
+					cr=GetColor(0,0,100);
+					break;
+				default:
+					cr=GetColor(0,0,0);
+					break;
+				}
+				DrawBox(20*i,20*j,20*(i+1),20*(j+1),cr,1);
+				cr=GetColor(255,255,255);
+				//DrawBox(20*i,20*j,20*(i+1),20*(j+1),cr,0);
 				DrawFormatString(i*20+5,j*20+5,cr,"%d",stage[i][j]);
-				DrawBox(20*i,20*j,20*(i+1),20*(j+1),cr,0);
+				
 			}
+		}
+		for(int i=0;i<AI_NUM;i++){
+			DrawBox(20*(ai_x[i]-10),20*(ai_y[i]-10),20*(ai_x[i]+10),20*(ai_y[i]+10),GetColor(255,255,0),0);
 		}
 		WaitTimer(100);
 	}
